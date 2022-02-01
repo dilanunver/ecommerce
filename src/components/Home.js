12496b44e3c6a113811f2c664f4e59c183be1548
dilanunver <dunver33@gmail.com>
2022-02-01 17:00:25 +0100

    react-responsive-carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,31 @@
 import React from "react";
 import homePhoto from '../pictures/homephoto.png'
+import homePhoto2 from '../pictures/homephoto2.png'
+import homePhoto3 from '../pictures/homephoto3.png'
+import homePhoto4 from '../pictures/homephoto4.png'
+
+
 import '../companents.css/home.css'
 import Products from "./Products";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Home = () => {
 
-
-
-
   return (
-    <div>
-      <div className="home-photo">
-        <img src={homePhoto} alt="homephoto" ></img>
-      </div>
-      <Products></Products>
+    <Carousel autoPlay infiniteLoop showThumbs={false}>
+      <div>
+        <div className="home-photo">
+          <img src={homePhoto} alt="homephoto" ></img>
+          <img src={homePhoto2} alt="homephoto" ></img>
+          <img src={homePhoto3} alt="homephoto" ></img>
+          <img src={homePhoto4} alt="homephoto" ></img>
+        </div>
+        <Products></Products>
 
-    </div>
+      </div>
+    </Carousel>
   )
 
 }
