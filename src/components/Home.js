63c71d9cbbbdc99c434423b6773d8d37a13224fe
dilanunver <dunver33@gmail.com>
2022-02-03 18:18:45 +0100

    fixing the carousel css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import homePhoto from '../pictures/homephoto.png'
 import homePhoto2 from '../pictures/homephoto2.png'
 import homePhoto3 from '../pictures/homephoto3.png'
 import homePhoto4 from '../pictures/homephoto4.png'
@@ -9,16 +8,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 
+
 const Home = () => {
 
   return (
     <div>
       <Carousel showThumbs={false} autoPlay infiniteLoop className="carousel-photo">
-
-        <div className="home-photo"> <img src={homePhoto} alt="homephoto" ></img> </div>
-        <div className="home-photo"> <img src={homePhoto2} alt="homephoto" ></img> </div>
-        <div className="home-photo"><img src={homePhoto3} alt="homephoto"  ></img> </div>
-        <div className="home-photo"><img src={homePhoto4} alt="homephoto"  ></img> </div>
+        <div className="home-photo"> <img src={homePhoto2} alt="homephoto" ></img></div>
+        <div className="home-photo"><img src={homePhoto3} alt="homephoto"  ></img></div>
+        <div className="home-photo"><img src={homePhoto4} alt="homephoto"  ></img></div>
 
       </Carousel>
       <Products></Products>
